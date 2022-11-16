4e28ebd32eb14657ce81d57de1eab09eb5c5c5b0
JRamDev <b.jairam0512@gmail.com>
2022-11-16 23:48:33 +0530

    knn using python

--- MLCourseraPrograms/programs/supervised_learning_programs/one.py
+++ MLCourseraPrograms/programs/supervised_learning_programs/one.py
@@ -1,8 +1,9 @@
+from cProfile import label
 import numpy as np
 import matplotlib.pyplot as plt
 
-x_train = np.array([1, 2])
-y_train = np.array([200, 350])
+x_train = np.array([1,2,3,4,5,6,7,8,9,10,11,12])
+y_train = np.array([58, 60, 80, 82, 82, 85, 88, 87, 85, 88, 70, 50])
 
 m = len(x_train)
 print(f"Number of training examples {m}")
@@ -14,8 +15,8 @@ y_i = y_train[i]
 
 plt.scatter(x_train, y_train, marker='x', c='r')
 plt.title('rice prices')
-plt.ylabel('Price in 1000s')
-plt.xlabel('Kgs')
+plt.ylabel('Price/kg')
+plt.xlabel('months')
 plt.show()
 
 # initial values
@@ -24,8 +25,21 @@ b = 100
 
 def calculate_model_output(w, b, x):
     m = x.shape
-    f_wb = np.zeroes(m)
+    f_wb = np.zeros(m)
     for i in range(len(x)):
         f_wb[i] = w * x[i] + b
 
     return f_wb
+
+temp_f_wb = calculate_model_output(w,b,x_train)
+plt.plot(x_train, temp_f_wb, c='b', label = 'Our Prediction')
+plt.scatter(x_train, y_train, marker='x', c='r', label = 'Actual Values')
+plt.title('rice prices')
+plt.ylabel('Price/kg')
+plt.xlabel('months')
+plt.legend()
+plt.show()
+
+# prediction
+w = 100
+b = 200
